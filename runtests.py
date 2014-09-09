f306347991eaedc932230ff72bc45b8afac104ee
SilentSokolov <silentsokolov@gmail.com>
2014-09-09 20:15:12 +0400

    fix

--- runtests.py
+++ runtests.py
@@ -7,13 +7,21 @@ from django.core.management import call_command
 
 settings.configure(
     MIDDLEWARE_CLASSES=global_settings.MIDDLEWARE_CLASSES,
-    INSTALLED_APPS=global_settings.INSTALLED_APPS,
+    INSTALLED_APPS=(
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sites',
+        'django.contrib.admin',
+        'django.contrib.sessions',
+        'admin_permissions',
+    ),
     DATABASES={
         'default': {'ENGINE': 'django.db.backends.sqlite3'}
     }
 )
 
 from django.test.utils import setup_test_environment
+
 setup_test_environment()
 
 if django.VERSION > (1, 7):
